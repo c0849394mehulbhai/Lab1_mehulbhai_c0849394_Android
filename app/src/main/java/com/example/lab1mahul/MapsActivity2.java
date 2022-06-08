79a2d35ceaf85114756895b7683951f4109271a1
mehulbhai <c0849394@mylambton.ca>
2022-06-08 00:41:46 -0400

    add marker and draw polygon

--- app/src/main/java/com/example/lab1mahul/MapsActivity2.java
+++ app/src/main/java/com/example/lab1mahul/MapsActivity2.java
@@ -1,51 +1,91 @@
 package com.example.lab1mahul;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.lab1mahul.databinding.ActivityMaps2Binding;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback {
+    int Red=0, Green=0, Blue=0;
 
-    private GoogleMap mMap;
+    Button btnDraw,btnClear;
+    private GoogleMap gMap;
     private ActivityMaps2Binding binding;
+
+    Polygon polygon = null;
+    List<LatLng> latLngList = new ArrayList<>();
+    List<Marker> markerList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps2);
 
-        binding = ActivityMaps2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        btnDraw = findViewById(R.id.btnDraw);
+        btnClear = findViewById(R.id.btnClear);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        btnDraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (polygon != null) {
+                    polygon.remove();
+                }
+                PolygonOptions polygonOptions = new PolygonOptions().clickable(true).addAll(latLngList);
+                polygon = gMap.addPolygon(polygonOptions);
+                polygon.setStrokeColor(Color.RED);
+                polygon.setFillColor(Color.GREEN);
+
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(polygon != null) polygon.remove();
+                for(Marker m : markerList) {
+                    m.remove();
+                }
+                latLngList.clear();
+                markerList.clear();
+            }
+        });
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        gMap = googleMap;
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+                Marker marker = gMap.addMarker(markerOptions);
+                latLngList.add(latLng);
+                markerList.add(marker);
+            }
+        });
     }
+
+
 }
